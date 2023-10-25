a2b49b90fa521bde4b35e2b472710325b7cada6a
CT-MarkLin <52391225+CT-MarkLin@users.noreply.github.com>
2023-10-26 07:26:33 +0800

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Audio } from './Audio';
-import { book } from './book2';
+import { book } from './book3';
 import './App.css';
 
 function heightToTop(ele: HTMLElement) {
